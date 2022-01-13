29796334155962ba029a57ba769977f99c226418
JavadMh <javadprogrammer10@gmail.com>
2022-01-13 20:14:37 +0330

    Fixing responsiveness issues and clean up the stylesheet.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-const navigationDrawer = document.getElementById("navigationDrawer");
+const header = document.getElementById("header");
+const navigationDropdown = document.getElementById("navigationDropdown");
 const drawerTogglerIcon = document.getElementById("drawerTogglerIcon");
-const mobileBodyOverly = document.getElementById("mobile-body-overly");
 
+// windows events
 window.onload = function () {
   // Navigation Menu
   drawerTogglerIcon.addEventListener("click", () => {
@@ -9,13 +10,13 @@ window.onload = function () {
   });
   // Dropdown
   const dropdowns = document.querySelectorAll(".dropdown");
-  if (dropdowns === undefined || dropdowns === null) {
+  if (dropdowns === null) {
     return;
   }
   dropdowns.forEach((dropdown) => {
     const dropdown_header =
       dropdown.getElementsByClassName("dropdown__header")[0];
-    if (dropdown_header === undefined || dropdown_header === null) {
+    if (dropdown_header === null) {
       return;
     }
     dropdown_header.addEventListener("click", (e) => {
@@ -24,8 +25,9 @@ window.onload = function () {
   });
 };
 
+// navigation menu handlers
 function toggleDrawer() {
-  navigationDrawer.classList.toggle("open");
+  header.classList.toggle("expanded-nav-menu");
+  navigationDropdown.classList.toggle("open");
   drawerTogglerIcon.classList.toggle("bx-x");
-  mobileBodyOverly.classList.toggle("show");
 }
